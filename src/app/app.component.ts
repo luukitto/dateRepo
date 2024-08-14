6d7d73f174eb1ed35da0769f8afac2b5b9cf3fa0
luka lobzhanidze <luukitto@gmail.com>
2024-08-14 15:36:56 +0400

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [
+    NgIf
+  ],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dateask';
+  showEmoji = false;
+
+  onYesClick() {
+    this.showEmoji = true;
+  }
 }
