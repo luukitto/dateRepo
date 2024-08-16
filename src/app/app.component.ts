35d4c3af671e36ab6e397aa29d05f16fcc03112b
luka lobzhanidze <luukitto@gmail.com>
2024-08-16 21:26:17 +0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
 import {NgIf} from "@angular/common";
 
 @Component({
@@ -13,7 +13,20 @@ import {NgIf} from "@angular/common";
 export class AppComponent {
   showEmoji = false;
 
+  @ViewChild('noButton', { static: false }) noButton!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
   onYesClick() {
     this.showEmoji = true;
+  }
+
+  onNoClick() {
+    const randomX = Math.floor(Math.random() * (window.innerWidth - 100));
+    const randomY = Math.floor(Math.random() * (window.innerHeight - 100));
+
+    this.renderer.setStyle(this.noButton.nativeElement, 'position', 'absolute');
+    this.renderer.setStyle(this.noButton.nativeElement, 'left', `${randomX}px`);
+    this.renderer.setStyle(this.noButton.nativeElement, 'top', `${randomY}px`);
   }
 }
